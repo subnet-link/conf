f3ead5677b460f96eb556d9d00bf4fad7008f0f4
subnet-link <contact@subnet.link>
2015-10-06 16:16:42 -0500

    Update run-once.sh

--- run-once.sh
+++ run-once.sh
@@ -5,7 +5,7 @@
 apt-get update; apt-get dist-upgrade -y; apt-get autoremove -y --purge; apt-get autoclean
 
 # Install packages
-apt-get install dc htop tcpdump
+apt-get install -y dcfldd htop iptables iptables-persistent tcpdump
 
 # Include bash.aliases file in bash.bashrc
 echo 'if [ -f /etc/bash.aliases ]; then echo ". /etc/bash.aliases"; fi' >> /etc/bash.bashrc
