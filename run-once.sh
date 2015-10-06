801a62050d437846b793e26971a657aefe0a6e9c
subnet-link <contact@subnet.link>
2015-10-06 16:22:09 -0500

    Update run-once.sh

--- run-once.sh
+++ run-once.sh
@@ -7,5 +7,6 @@ apt-get update; apt-get dist-upgrade -y; apt-get autoremove -y --purge; apt-get 
 # Install packages
 apt-get install -y curl dcfldd htop iptables iptables-persistent tcpdump wget
 
-# Include bash.aliases file in bash.bashrc
+# Download and include bash.aliases file in bash.bashrc
+wget --secure-protocol=TLSv1 --https-only -O /etc/bash.aliases https://raw.githubusercontent.com/subnet-link/conf/master/bash.aliases
 echo 'if [ -f /etc/bash.aliases ]; then echo ". /etc/bash.aliases"; fi' >> /etc/bash.bashrc
