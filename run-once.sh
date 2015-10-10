33aa09eccdb6ef572d2af1f2a2f6442ccab0a7b1
subnet-link <contact@subnet.link>
2015-10-10 16:25:51 -0500

    Update run-once.sh
    Added download of Hack font.

--- run-once.sh
+++ run-once.sh
@@ -10,3 +10,11 @@ apt-get install -y curl dcfldd htop iptables iptables-persistent tcpdump wget
 # Download and include bash.aliases file in bash.bashrc
 wget --secure-protocol=TLSv1 --https-only -O /etc/bash.aliases https://raw.githubusercontent.com/subnet-link/conf/master/bash.aliases
 echo 'if [ -f /etc/bash.aliases ]; then echo ". /etc/bash.aliases"; fi' >> /etc/bash.bashrc
+
+# Download and include Hack font
+wget --secure-protocol=TLSv1 --https-only -O /tmp/hack.zip https://github.com/chrissimpkins/Hack/releases/download/v2.015/Hack-v2_015-ttf.zip
+mkdir -pv /usr/share/fonts/truetype/hack
+unzip /tmp/hack.zip -d /usr/share/fonts/truetype/hack
+fc-cache -vf 
+rm -vf /tmp/hack.zip
+
